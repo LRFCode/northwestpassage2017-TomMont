198eea7e4040ef4e00bd6627a4cfec9b0b04f571
Louis <lrflrf@sbcglobal.net>
2017-06-12 09:29:23 -0500

    add platinum

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -26,12 +26,18 @@ import play.mvc.Result;
 //TODO Make the western route have a "Meeting with Santa" page that
 //TODO   the player has a 50% chance of seeing
 //TODO Use the random user API to get names for the crew members at the start of the game
+//TODO   store the crew member names in session
 //TODO On each screen show the list of able bodied crew and announce
 //TODO   the name of the injured crew person
 //TODO Consolidate the end game pages into a single page to reduce duplicate code
 //TODO Add an Easter Egg (Google or use search engine of your choice for "Easter Egg Game"
 //TODO   if an explanation is needed)
 
+//PLATINUM
+//TODO On the welcome screen allow the user to pick one of four pictures of a kitten
+//TODO   to represent themselves.  Show this image on each screen of the game.
+//TODO Implement a 20% chance that the fidget spinners are covered in lead paint
+//TODO   When this happens the Queen should throw the player in prison
 
 public class GameController extends Controller
 {
