6fa1382a902f31cc33288ffe32a90dee0924713b
tmontgomery <businessmontgomery@gmail.com>
2017-09-28 12:31:51 -0500

    Some Silver Completed

--- app/controllers/GameController.java
+++ app/controllers/GameController.java
@@ -1,7 +1,11 @@
 package controllers;
 
+import play.data.DynamicForm;
+import play.data.FormFactory;
 import play.mvc.Controller;
 import play.mvc.Result;
+
+import javax.inject.Inject;
 
 //BRONZE
 //TODONE Add a Play Again button to the pages that end the game. It should go to the start page.
@@ -11,8 +15,8 @@ import play.mvc.Result;
 //TODONE   Make up your own description for this page. G rated please.
 
 //SILVER
-//TODO Add a text box to the welcome page for the player to enter their name
-//TODO Inside the start method store the player name in session
+//TODONE Add a text box to the welcome page for the player to enter their name
+//TODONE Inside the start method store the player name in session
 //TODO On the game end pages change "The end." to say "The end for #playerName#."
 //TODO Make the player lose one crew member due to injury on each turn.
 //TODO   HINT: Store the number of able bodied crew in session
@@ -39,6 +43,15 @@ import play.mvc.Result;
 
 public class GameController extends Controller
 {
+    FormFactory formFactory;
+
+    @Inject
+    public GameController(FormFactory formFactory)
+    {
+        this.formFactory = formFactory;
+
+    }
+
     public Result welcome()
     {
         return ok(views.html.welcome.render());
@@ -46,7 +59,10 @@ public class GameController extends Controller
 
     public Result start()
     {
-        return ok(views.html.start.render());
+        DynamicForm dynamicForm = formFactory.form().bindFromRequest();
+        String name = dynamicForm.get("name");
+        session().put("name", name);
+        return ok(views.html.start.render(name));
     }
 
     public Result goEastFromEngland()
@@ -66,12 +82,14 @@ public class GameController extends Controller
 
     public Result eastEnd()
     {
-        return ok(views.html.eastend.render());
+        String name = session().get("name");
+        return ok(views.html.eastend.render(name));
     }
 
     public Result westEnd()
     {
-        return ok(views.html.westend.render());
+        String name = session().get("name");
+        return ok(views.html.westend.render(name));
     }
 
     public Result homePort()
@@ -88,4 +106,7 @@ public class GameController extends Controller
     {
         return ok(views.html.earlynorth.render());
     }
+
+
 }
+
